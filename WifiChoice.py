a27de9cb8576b7a0b0e23232ed72c83f167e959d
saladxuanzhi <2513439510@qq.com>
2022-09-29 15:29:44 +0800

    Add function of wifi automatic connection

---
+++ WifiChoice.py
@@ -0,0 +1,89 @@
+from pywifi import PyWiFi
+from pywifi import const
+import time
+import os
+import re
+
+# 0 const.IFACE_DISCONNECTED
+# 1 const.IFACE_SCANNING
+# 2 const.IFACE_INACTIVE
+# 3 const.IFACE_CONNECTING
+# 4 const.IFACE_CONNECTED
+
+class WifiChoice(object):
+    def __init__(self) -> None:
+        pass
+
+    def getCurrentSSID(self):
+        cmd = 'netsh wlan show interfaces'
+        result = os.popen(cmd)
+        text = result.read().split('\n')
+        result.close()
+        for i in text:
+            if 'SSID' in i :
+                ret = i.strip('\r').replace(' ','')
+                break
+        return ret[5:]
+
+    def getWifiProfile(self):
+        cmd = 'netsh wlan show profile'
+        result = os.popen(cmd)
+        text = result.read()
+        result.close()
+        lst = text.split('\n')
+        ssid = []
+        for i in range(len(lst)):
+            t = re.search("(?<= : ).*", lst[i])
+            if t is not None:
+                ssid.append(t[0].encode('utf-8').decode('ansi'))
+        return ssid
+
+    def getAvailableWifi(self):
+        iface = PyWiFi().interfaces()[0]
+        iface.scan()
+        time.sleep(3)
+
+    def getStatus(self):
+        iface = PyWiFi().interfaces()[0]
+        i = 5
+        while i > 0:
+            if iface.status() == const.IFACE_CONNECTED:
+                return 1
+            elif iface.status() == const.IFACE_DISCONNECTED:
+                return 0
+            elif iface.status() == const.IFACE_SCANNING or iface.status() == const.IFACE_CONNECTING:
+                i -= 1
+                time.sleep(2)
+            elif iface.status() == const.IFACE_INACTIVE:
+                return -1
+        return -1
+
+    def connect(self, ssid):
+        cmd = 'netsh wlan connect name="%s"' % ssid
+        result = os.popen(cmd)
+        text = result.read()
+        result.close()
+        text = text.strip('\n').encode('utf-8').decode('ansi')
+        return text
+
+    def makeChoice(self):
+        status = self.getStatus()
+        if status == -1:
+            return 0, 'Inactive or unstable network!'
+        if status:
+            if 'NWAFU' not in self.getCurrentSSID():
+                return 0, 'Free network connected.'
+            return 2, 'Check camp net.'
+        wifi_list = self.getWifiProfile()
+        if 'NWAFU' in wifi_list:
+            wifi_list.remove('NWAFU')
+        if 'NWAFU-HW' in wifi_list:
+            wifi_list.remove('NWAFU-HW')
+        available_list = self.getAvailableWifi()
+        for ssid in available_list:
+            if ssid in wifi_list:
+                return 1, ssid
+        if 'NWAFU' in available_list:
+            return 1, 'NWAFU'
+        if 'NWAFU-HW' in available_list:
+            return 1, 'NWAFU-HW'
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+import time
 import CampNet
+import WifiChoice
 
 username = '' #学号
 password = ''
 
-a = CampNet.IfLogin(username, password)
-if a.getStatus():
-    msg = a.doLogin()
-    if msg[1]:
-        print('Successful!')
-    else:
-        print('Failed!')
-    print(msg[0])
+def NWAFU():
+    a = CampNet.IfLogin(username, password)
+    if a.getStatus():
+        msg = a.doLogin()
+        if msg[1]:
+            print('Successful!')
+        else:
+            print('Failed!')
+        print(msg[0])
+
+def main_work():
+    wifi = WifiChoice()
+    choice, msg = wifi.makeChoice()
+    if not choice:
+        print(msg)
+        return -1
+    if choice == 1:
+        print(wifi.connect(msg))
+        if 'NWAFU' in msg:
+            NWAFU()
+        return 0
+    if choice == 2:
+        NWAFU()
+        return 0
+
+if __name__ == '__main__':
+    while True:
+        main_work()
+        time.sleep(10)
